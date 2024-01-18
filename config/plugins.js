325aa442475de6c5822e362ae7e673a2a1cc4f1e
Nicolas Alonso <nick-aj@Nicolass-MacBook-Pro.local>
2024-01-18 14:28:48 +0200

    init

---
+++ config/plugins.js
@@ -0,0 +1,29 @@
+// module.exports = ({ env }) => ({
+//     // … some unrelated plugins configuration options
+//     upload: {
+//        config: {
+//           // … provider-specific upload configuration options go here
+//        }
+//     // … some other unrelated plugins configuration options
+//     }
+//     });
+
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
